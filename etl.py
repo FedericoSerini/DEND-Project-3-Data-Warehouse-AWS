f410bbf5be15ac749137b24fcc585da2fc5f7bb8
FedericoSerini <roacefg@gmail.com>
2019-04-25 11:02:05 +0200

    initial commit

---
+++ etl.py
@@ -0,0 +1,36 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    for query in copy_table_queries:
+        print('Loading data by: '+query)
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    print('Connecting to redshift')
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    print('Connected to redshift')
+    cur = conn.cursor()
+    
+    print('Loading staging tables')
+    load_staging_tables(cur, conn)
+    #insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sql_queries.py
@@ -0,0 +1,162 @@
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+# DROP TABLES
+
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+staging_events_table_create = ("""CREATE TABLE staging_events(
+    event_id INT IDENTITY(0,1),
+    artist_name VARCHAR(255),
+    auth VARCHAR(50),
+    user_first_name VARCHAR(255),
+    user_gender  VARCHAR(1),
+    item_in_session	INTEGER,
+    user_last_name VARCHAR(255),
+    song_length	DOUBLE PRECISION, 
+    user_level VARCHAR(50),
+    location VARCHAR(255),	
+    method VARCHAR(25),
+    page VARCHAR(35),	
+    registration VARCHAR(50),	
+    session_id	BIGINT,
+    song_title VARCHAR(255),
+    status INTEGER,  -- SHOULD BE ALWAYS A NUMBER, HTTP STATUS	
+    ts VARCHAR(50),
+    user_agent TEXT,	
+    user_id VARCHAR(100),
+    PRIMARY KEY (event_id))
+""")
+
+staging_songs_table_create = ("""CREATE TABLE staging_songs(
+    song_id VARCHAR(100),
+    num_songs INTEGER,
+    artist_id VARCHAR(100),
+    artist_latitude DOUBLE PRECISION,
+    artist_longitude DOUBLE PRECISION,
+    artist_location VARCHAR(255),
+    artist_name VARCHAR(255),
+    title VARCHAR(255),
+    duration DOUBLE PRECISION,
+    year INTEGER,
+    PRIMARY KEY (song_id))
+""")
+
+songplay_table_create = ( """CREATE TABLE songplays(
+    songplay_id INT IDENTITY(0,1),
+    start_time TIMESTAMP REFERENCES time(start_time),
+    user_id VARCHAR(50) REFERENCES users(user_id),
+    level VARCHAR(50),
+    song_id VARCHAR(100) REFERENCES songs(song_id),
+    artist_id VARCHAR(100) REFERENCES artists(artist_id),
+    session_id BIGINT,
+    location VARCHAR(255),
+    user_agent TEXT,
+    PRIMARY KEY (songplay_id))
+""")
+
+user_table_create = ("""CREATE TABLE users(
+    user_id VARCHAR,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    gender VARCHAR(1),
+    level VARCHAR(50),
+    PRIMARY KEY (user_id))
+""")
+
+song_table_create = ("""CREATE TABLE songs(
+    song_id VARCHAR(100),
+    title VARCHAR(255),
+    artist_id VARCHAR(100),
+    year INTEGER,
+    duration DOUBLE PRECISION,
+    PRIMARY KEY (song_id))
+""")
+
+artist_table_create = ("""CREATE TABLE artists(
+    artist_id VARCHAR(100),
+    name VARCHAR(255),
+    location VARCHAR(255),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    PRIMARY KEY (artist_id))
+""")
+
+time_table_create = ("""CREATE TABLE time(
+    start_time TIMESTAMP,
+    hour INTEGER,
+    day INTEGER,
+    week INTEGER,
+    month INTEGER,
+    year INTEGER,
+    weekday INTEGER,
+    PRIMARY KEY (start_time))
+""")
+
+# STAGING TABLES
+
+# Load from JSON Arrays Using a JSONPaths file (LOG_JSONPATH),
+# setting COMPUPDATE, STATUPDATE to speed up COPY
+staging_events_copy = ("""copy staging_events from '{}'
+ credentials 'aws_iam_role={}'
+ region 'us-west-2' 
+ COMPUPDATE OFF STATUPDATE OFF
+ JSON '{}'""").format(config.get('S3','LOG_DATA'),
+                        config.get('IAM_ROLE', 'ARN'),
+                        config.get('S3','LOG_JSONPATH'))
+
+# setting COMPUPDATE, STATUPDATE to speed up COPY
+staging_songs_copy = ("""copy staging_songs from '{}'
+    credentials 'aws_iam_role={}'
+    region 'us-west-2' 
+    COMPUPDATE OFF STATUPDATE OFF
+    JSON 'auto'
+    """).format(config.get('S3','SONG_DATA'), 
+                config.get('IAM_ROLE', 'ARN'))
+
+# FINAL TABLES
+
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s) 
+""")
+
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) 
+    ON CONFLICT (user_id) DO UPDATE SET firstName=users.first_name, lastName=users.last_name, gender=users.gender, level=users.level
+""")
+
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) DO UPDATE SET title=songs.title, artist_id=songs.artist_id,
+    year=songs.year, duration=songs.duration 
+""")
+
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO UPDATE SET name=artists.name, location=artists.location, latitude=artists.latitude, 
+    longitude=artists.longitude 
+""")
+
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) DO UPDATE SET hour=time.hour, day=time.day, week=time.week, month=time.month, 
+    year=time.year, weekday=time.weekday 
+""")
+
+# QUERY LISTS
+
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+
+
+
